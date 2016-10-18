1e46658a0666a4d8b3c9fd46404aef75406e0c66
Alex Naidis <alex.naidis@linux.com>
2016-10-18 15:18:47 +0200

    Enable Arithmetic Encoding/Decoding
    This commit adds the needed files for the
    Arithmetic Encoder/Decoder to compilation
    and enables Arithmetic Encoding/Decoding support
    in the proper configuration file.
    
    Test: Displaying appropriate JPEG files.
    
    Change-Id: I1745464a9af46ac8abfe6722f666589cb82ef59d
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- Android.mk
+++ Android.mk
@@ -31,14 +31,15 @@ endif
 
 
 libjpeg_turbo_common_src_files := \
-    jcapimin.c jcapistd.c jccoefct.c jccolor.c jcdctmgr.c jchuff.c \
-    jcinit.c jcmainct.c jcmarker.c jcmaster.c jcomapi.c jcparam.c \
-    jcphuff.c jcprepct.c jcsample.c jctrans.c jdapimin.c jdapistd.c \
-    jdatadst.c jdatasrc.c jdcoefct.c  jdcolor.c jddctmgr.c jdhuff.c \
-    jdinput.c jdmainct.c jdmarker.c jdmaster.c jdmerge.c jdphuff.c \
-    jdpostct.c jdsample.c jdtrans.c jerror.c jfdctflt.c jfdctfst.c \
-    jfdctint.c jidctflt.c jidctfst.c jidctint.c jidctred.c jmemmgr.c \
-    jmemnobs.c jquant1.c jquant2.c jutils.c
+    jcapimin.c jcapistd.c jaricom.c jcarith.c jccoefct.c jccolor.c \
+    jcdctmgr.c jchuff.c jcinit.c jcmainct.c jcmarker.c jcmaster.c \
+    jcomapi.c  jcparam.c jcphuff.c jcprepct.c jcsample.c jctrans.c \
+    jdapimin.c  jdapistd.c jdarith.c jdatadst.c jdatasrc.c jdcoefct.c \
+    jdcolor.c jddctmgr.c jdhuff.c jdinput.c jdmainct.c jdmarker.c \
+    jdmaster.c jdmerge.c jdphuff.c  jdpostct.c jdsample.c jdtrans.c \
+    jerror.c jfdctflt.c jfdctfst.c jfdctint.c jidctflt.c jidctfst.c \
+    jidctint.c jidctred.c jmemmgr.c jmemnobs.c jquant1.c jquant2.c \
+    jutils.c
 
 # ARM v7 NEON
 libjpeg_turbo_common_src_files_arm := simd/jsimd_arm_neon.S simd/jsimd_arm.c
