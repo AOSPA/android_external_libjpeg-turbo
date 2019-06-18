4609b0acaf50dfd97a62f38916fba0abbeb70e00
Jonathan Wright <jonathan.wright@arm.com>
2019-06-18 16:07:52 +0100

    Implement slow IDCT using Arm NEON intrinsics
    Adds an Arm NEON intrinsics implementation of the 'slow' accurate
    inverse discrete cosine transform (IDCT).
    
    The NEON assembly implementations are removed for both AArch32 and
    AArch64.
    
    Bug: 922430
    Change-Id: Ic35eb8f0e5dee0a8a1f71b9ffc303be5e2d3e392

--- simd/arm/jsimd.c
+++ simd/arm/jsimd.c
@@ -18,6 +18,7 @@
 
 #define JPEG_INTERNALS
 #include "jdsample-neon.h"
+#include "jidctint-neon.h"
 #include "jidctred-neon.h"
 
 #include <stdio.h>
--- simd/arm64/jsimd.c
+++ simd/arm64/jsimd.c
@@ -17,6 +17,7 @@
 
 #define JPEG_INTERNALS
 #include "../arm/jdsample-neon.h"
+#include "../arm/jidctint-neon.h"
 #include "../arm/jidctred-neon.h"
 
 #include <stdio.h>
